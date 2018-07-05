23c6aa5070256e69455b1931e03f8da085385331
ngt kn <nagata.ken78@gmail.com>
2018-07-05 09:36:01 -0700

    initial commit

---
+++ app/src/main/java/com/ngtkn/customalertdialog/MainActivity.java
@@ -0,0 +1,40 @@
+package com.ngtkn.customalertdialog;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDialog(View view) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Close");
+        builder.setMessage("Are you sure?");
+        builder.setIcon(R.drawable.ic_close_black_24dp);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+}
